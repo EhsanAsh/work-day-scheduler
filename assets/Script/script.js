d99ab30665d074cf7634ead2f7435a7cec56626f
EhsanAsh <ehsan.ashrafipour@gmail.com>
2023-08-08 17:00:28 -0400

    add two events and functions to get user's input and increase the UX

--- assets/Script/script.js
+++ assets/Script/script.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
 
+  let timeBlock = $(".timeBlock");
   let currentDay = $("#currentDay");
+  let textDiv = $(".textDiv");
+  
   // Displaying current date and time.
   const timeCounter = function () {
     let today = dayjs().format('dddd, MMMM D YYYY, h:mm:ss a');
@@ -11,7 +14,6 @@ $(document).ready(function () {
   const allTimeBls = function () {
 
     let currentHour = dayjs().hour();
-    let timeBlock = $(".timeBlock");
     // I'm trying to loop through each time block, get each Time block's id, and compare it to the current hour.
     // Used (https://learn.jquery.com/using-jquery-core/iterating/) documentation as a reference.
     timeBlock.each(function () {
@@ -29,9 +31,34 @@ $(document).ready(function () {
     });
 
   };
-  allTimeBls();
+
+  // Here I'm trying to change the text area to an input field when clicked and vice versa to get user input, and make a bettter UX.
+  // Used(https://learn.jquery.com/events/event-basics/), (https://learn.jquery.com/using-jquery-core/attributes/) as a reference.
+  const txtToInput = function () {
+
+    let userTxt = $(this).val().trim();
+    let txtInput = $("<input>").addClass("description form-control").val(userTxt).attr("type", "text");
+    $(this).replaceWith(txtInput);
+    txtInput.trigger("focus");
+
+  }
+
+  const inputToTxt = function () {
+
+    let inputValue = $(this).val().trim();
+    let newTxtArea = $("<textarea>").addClass("description form-control").val(inputValue).attr({ "rows": "3", "placeholder": "Write your notes here."});
+    $(this).replaceWith(newTxtArea);
+    allTimeBls();
+
+  }
 
   // Set interval to update time every second by using timeCounter function
   setInterval(timeCounter, 1000);
   
+  allTimeBls();
+
+  // Event listener for time blocks to change text area to input field and vice versa.
+  textDiv.on("click", "textarea", txtToInput);
+  textDiv.on("blur", "input", inputToTxt);
+
 });
